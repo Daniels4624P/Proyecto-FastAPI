cf506006872b76cec3eabb69297e89c1d45015e4
Daniels4624P <96382302+Daniels4624P@users.noreply.github.com>
2024-09-11 20:28:43 -0500

    Delete src directory

--- src/db/db.py
+++
@@ -1,8 +0,0 @@
-from pymongo import MongoClient
-
-URL_CONNECTION = "mongodb+srv://user:Gravityfalls@taskmanager.hcvxo.mongodb.net/?retryWrites=true&w=majority&appName=TaskManager"
-
-client = MongoClient(URL_CONNECTION)
-
-db_tasks = client.tasks_proyect_task_manager
-db_users = client.users_proyect_task_manager
